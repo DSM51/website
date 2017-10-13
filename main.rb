8b4c59e7f7d693f3a53cc32ca4edeef58012427d
Damian Rzeszot <damian.rzeszot@gmail.com>
2017-10-13 21:38:42 +0200

    use caching only in production

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 require 'haml'
 
 before do
-  cache_control :public, :must_revalidate, max_age: 5*60
+  cache_control :public, :must_revalidate, max_age: 60 if settings.production?
 end
 
 get '/' do
